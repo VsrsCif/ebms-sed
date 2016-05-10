dc4b0d04d6b6d7b8b628539efa4636ebefe38f81
VsrsCif <cif.uporabniki@sodisce.si>
2016-05-10 09:39:28 +0200

    Internacionalization  - eng. svn language

---
+++ ebms-sed-module/ebms-sed-web/src/main/java/si/sed/msh/web/gui/LocaleManager.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package si.sed.msh.web.gui;
+
+import java.util.Locale;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+@SessionScoped
+public class LocaleManager {
+
+    private Locale locale;
+
+    @PostConstruct
+    public void init() {
+        locale = FacesContext.getCurrentInstance().getExternalContext().getRequestLocale();
+    }
+
+    public Locale getLocale() {
+        return locale;
+    }
+
+    public String getLanguage() {
+        return locale.getLanguage();
+    }
+
+    public void setLanguage(String language) {
+        locale = new Locale(language);
+        FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
+    }
+
+}
